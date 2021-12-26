c9bd58892be3c6d8e99d0c936d94d288ebfa74f4
ckigen.k <ckigen.k@gmail.com>
2021-12-26 16:49:40 +0300

    updated links

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from "./Components/Home";
 import { Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Data from "./Components/Data";
-import AddSample from "./Components/AddSample";
+// import AddSample from "./Components/AddSample";
 // import NewHome from "./Components/NewHome";
 
 function App() {
@@ -14,8 +14,8 @@ function App() {
       <NavbarComp />
       <Routes>
         <Route path="/" element={<Home marginTop="40px" />}></Route>
-        <Route path="/1" element={<AddSample />}></Route>
-        <Route path="/data" element={<Data />}></Route>
+        <Route path="/1" element={<Data />}></Route>
+        {/* <Route path="/data" element={<Data />}></Route> */}
         {/* <Route path="/add" element={<AddSample />}></Route> */}
         {/* <Route path="/new" element={<NewHome />}></Route> */}
       </Routes>
